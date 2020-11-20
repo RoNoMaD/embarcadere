d62c3f420cae3d245b8c1464007cc484fccf6954
Romain Lebascle <romain.lebascle@gmail.com>
2020-11-20 10:38:02 +0100

    feat: add aticle schema object

---
+++ studio/schemas/objects/article.js
@@ -0,0 +1,43 @@
+export default {
+  type: 'object',
+  name: 'article',
+  title: 'Article',
+  fields: [
+    {
+      name: 'heading',
+      type: 'string',
+      title: 'Heading'
+    },
+    {
+      name: 'tagline',
+      type: 'simplePortableText',
+      title: 'Tagline'
+    },
+    {
+      name: 'image',
+      type: 'image',
+      title: 'Image',
+      options: {
+        hotspot: true
+      }
+    },
+    {
+      name: 'link',
+      type: 'internalLink',
+      title: 'Link'
+    }
+  ],
+  preview: {
+    select: {
+      title: 'article',
+      media: 'image'
+    },
+    prepare({ title, media }) {
+      return {
+        title,
+        subtitle: 'Article section',
+        media
+      }
+    }
+  }
+}

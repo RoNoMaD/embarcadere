b1b88c89d3af3b9a4db28d7e07302eb2ae31daa4
Romain Lebascle <romain.lebascle@gmail.com>
2020-11-28 10:40:42 +0100

    fix: use harry roberts font css optimal loading strategy

--- web/components/Layout.js
+++ web/components/Layout.js
@@ -31,11 +31,46 @@ function Layout(props) {
           name="viewport"
           content="initial-scale=1.0, width=device-width, viewport-fit=cover"
         />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        {/*
+  - 1. Preemptively warm up the fonts’ origin.
+  -
+  - 2. Initiate a high-priority, asynchronous fetch for the CSS file. Works in
+  -    most modern browsers.
+  -
+  - 3. Initiate a low-priority, asynchronous fetch that gets applied to the page
+  -    only after it’s arrived. Works in all browsers with JavaScript enabled.
+  -
+  - 4. In the unlikely event that a visitor has intentionally disabled
+  -    JavaScript, fall back to the original method. The good news is that,
+  -    although this is a render-blocking request, it can still make use of the
+  -    preconnect which makes it marginally faster than the default.
+        */}
+
+        {/* <!-- [1] --> */}
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+
+        {/* <!-- [2] --> */}
         <link
-          href="https://fonts.googleapis.com/css2?family=Lato&family=Oswald:wght@400;500&display=swap"
-          rel="stylesheet"
+          rel="preload"
+          as="style"
+          href="https://fonts.googleapis.com/css2?family=Lato&family=Oswald:wght@400;500&&display=swap"
         />
+
+        {/* <!-- [3] --> */}
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Lato&family=Oswald:wght@400;500&&display=swap"
+          media="print"
+          onload="this.media='all'"
+        />
+
+        {/* <!-- [4] --> */}
+        <noscript>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Lato&family=Oswald:wght@400;500&&display=swap"
+          />
+        </noscript>
       </Head>
       <div className="container">
         <Header title={title} navItems={mainNavigation} logo={logo} />

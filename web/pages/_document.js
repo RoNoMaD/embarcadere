ab90807dd6e6d127a83c08c046125a5e20513ace
Romain Lebascle <romain.lebascle@gmail.com>
2020-11-30 14:45:45 +0100

    fix: add writing direction on html root tag

--- web/pages/_document.js
+++ web/pages/_document.js
@@ -1,23 +1,9 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
-// import { i18n } from "../i18n";
-
 class MyDocument extends Document {
-  // static async getInitialProps(ctx) {
-  //   const initialProps = await Document.getInitialProps(ctx);
-  //   const slugs = ctx.req.url.split("/").filter((slug) => slug);
-  //   console.log("slugs", slugs);
-  //   console.log("slugs", slugs);
-  //   const lang =
-  //     slugs.length > 0 && i18n.locales.includes(slugs[0])
-  //       ? slugs[0]
-  //       : i18n.defaultLocale;
-  //   return { ...initialProps, lang };
-  // }
-
   render() {
     return (
-      <Html lang="fr">
+      <Html lang="fr" dir="ltr">
         <Head />
         <body>
           <Main />

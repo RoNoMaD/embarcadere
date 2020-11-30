17701684152cbb6b48df3aad02ea38f846e670b8
Romain Lebascle <romain.lebascle@gmail.com>
2020-11-30 15:15:05 +0100

    feat: use preact instead of react

--- web/next.config.js
+++ web/next.config.js
@@ -1,13 +1,16 @@
 const withCSS = require("@zeit/next-css");
 
 const isProduction = process.env.NODE_ENV === "production";
+const withPreact = require("next-plugin-preact");
 
-module.exports = withCSS({
-  cssModules: true,
-  cssLoaderOptions: {
-    importLoaders: 1,
-    localIdentName: isProduction
-      ? "[hash:base64:5]"
-      : "[name]__[local]___[hash:base64:5]",
-  },
-});
+module.exports = withPreact(
+  withCSS({
+    cssModules: true,
+    cssLoaderOptions: {
+      importLoaders: 1,
+      localIdentName: isProduction
+        ? "[hash:base64:5]"
+        : "[name]__[local]___[hash:base64:5]",
+    },
+  })
+);

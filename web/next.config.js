436df899e3e7777d0a5a80b64e8a85077eb77aac
Romain Lebascle <romain.lebascle@gmail.com>
2020-11-30 17:34:50 +0100

    fix: remove preact

--- web/next.config.js
+++ web/next.config.js
@@ -1,16 +1,13 @@
 const withCSS = require("@zeit/next-css");
 
 const isProduction = process.env.NODE_ENV === "production";
-const withPreact = require("next-plugin-preact");
 
-module.exports = withPreact(
-  withCSS({
-    cssModules: true,
-    cssLoaderOptions: {
-      importLoaders: 1,
-      localIdentName: isProduction
-        ? "[hash:base64:5]"
-        : "[name]__[local]___[hash:base64:5]",
-    },
-  })
-);
+module.exports = withCSS({
+  cssModules: true,
+  cssLoaderOptions: {
+    importLoaders: 1,
+    localIdentName: isProduction
+      ? "[hash:base64:5]"
+      : "[name]__[local]___[hash:base64:5]",
+  },
+});

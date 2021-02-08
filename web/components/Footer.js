5517b8f6350c872241a9199bc0b75978fe6eb465
Romain Lebascle <romain.lebascle@gmail.com>
2021-02-08 11:49:55 +0100

    feat: lazy load the google map in the footer

--- web/components/Footer.js
+++ web/components/Footer.js
@@ -1,5 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { useIntersection } from "react-use";
+
 import SimpleBlockContent from "./SimpleBlockContent";
 
 import styles from "./Footer.module.css";
@@ -11,6 +13,13 @@ function Footer({
   footerAddress,
   footerSocialMedias,
 }) {
+  const intersectionRef = React.useRef(null);
+  const intersection = useIntersection(intersectionRef, {
+    root: null,
+    rootMargin: "0px",
+    threshold: 1,
+  });
+
   return (
     <div className={styles.root}>
       <div className={styles.sections}>
@@ -52,19 +61,28 @@ function Footer({
             Voir l'itinéraire
           </a>
         </div>
-        <div className={styles.section}>
-          <iframe
-            title="Carte"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2752.8230368265654!2d-0.7441696315865893!3d46.37289890966355!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4806da6375351a3f%3A0x6eca6fd9f3633121!2sEmbarcad%C3%A8re%20de%20l&#39;Abbaye!5e0!3m2!1sfr!2sfr!4v1606914485896!5m2!1sfr!2sfr"
-            width="300"
-            height="300"
-            frameBorder="0"
-            style={{ border: 0 }}
-            allowFullScreen
-            aria-hidden="false"
-            tabIndex="0"
-            loading="lazy"
-          ></iframe>
+        <div ref={intersectionRef} className={styles.section}>
+          {intersection && intersection.intersectionRatio < 1 ? (
+            <img
+              src="../images/embarcadere_map.png"
+              width="300"
+              height="300"
+              alt="carte de l'embarcadère"
+            />
+          ) : (
+            <iframe
+              title="Carte"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2752.8230368265654!2d-0.7441696315865893!3d46.37289890966355!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4806da6375351a3f%3A0x6eca6fd9f3633121!2sEmbarcad%C3%A8re%20de%20l&#39;Abbaye!5e0!3m2!1sfr!2sfr!4v1606914485896!5m2!1sfr!2sfr"
+              width="300"
+              height="300"
+              frameBorder="0"
+              style={{ border: 0 }}
+              allowFullScreen
+              aria-hidden="false"
+              tabIndex="0"
+              loading="lazy"
+            ></iframe>
+          )}
         </div>
       </div>
       <div className={styles.company}>

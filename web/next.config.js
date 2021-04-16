e2cfce7ccf2d11c6bf6b8363b78e3aa70b3f6ea8
Romain Lebascle <romain.lebascle@gmail.com>
2021-04-16 11:03:53 +0200

    feat: only use CSP in prod

--- web/next.config.js
+++ web/next.config.js
@@ -3,24 +3,28 @@ const { createSecureHeaders } = require("next-secure-headers");
 
 module.exports = {
   target: "serverless",
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: createSecureHeaders({
-          contentSecurityPolicy: {
-            directives: {
-              defaultSrc: ["'self'"],
-              styleSrc: ["'self'", "'unsafe-inline'", "fonts.googleapis.com"],
-              imgSrc: ["'self'", "cdn.sanity.io"],
-              fontSrc: ["'self'", "fonts.gstatic.com"],
-              baseUri: "self",
-              formAction: "self",
-              frameAncestors: true,
+  ...(process.env.NODE_ENV === "production" && {
+    // https://github.com/jagaapple/next-secure-headers#how-to-remove-x-powered-by-header
+    poweredByHeader: false,
+    async headers() {
+      return [
+        {
+          source: "/(.*)",
+          headers: createSecureHeaders({
+            contentSecurityPolicy: {
+              directives: {
+                defaultSrc: "'self'",
+                styleSrc: ["'self'", "'unsafe-inline'", "fonts.googleapis.com"],
+                imgSrc: ["'self'", "cdn.sanity.io"],
+                fontSrc: ["'self'", "fonts.gstatic.com"],
+                baseUri: "self",
+                formAction: "self",
+                frameAncestors: true,
+              },
             },
-          },
-        }),
-      },
-    ];
-  },
+          }),
+        },
+      ];
+    },
+  }),
 };
